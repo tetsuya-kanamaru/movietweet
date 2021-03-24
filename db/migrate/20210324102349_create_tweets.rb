adb4055a7c614db4ed2cd3e8a5f809d795560ef0
tetsuya_kanamaru <67969845+tetsuya-kanamaru@users.noreply.github.com>
2021-03-24 19:57:14 +0900

    create home page

--- db/migrate/20210324102349_create_tweets.rb
+++ db/migrate/20210324102349_create_tweets.rb
@@ -1,7 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[6.1]
   def change
     create_table :tweets do |t|
-
+      t.string :title, null: false
+      t.string :content
       t.timestamps
     end
   end

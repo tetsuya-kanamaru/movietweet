f317c44fc20d20ee3af4f14c68106fbdd8d8076a
tetsuya_kanamaru <67969845+tetsuya-kanamaru@users.noreply.github.com>
2021-03-26 16:46:53 +0900

    migrate comment table to DB

--- db/migrate/20210326074202_create_comments.rb
+++ db/migrate/20210326074202_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-
+      t.integer :user_id
+      t.integer :tweet_id
+      t.text :comment
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_015338) do
+ActiveRecord::Schema.define(version: 2021_03_26_074202) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 2021_03_26_015338) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tweets", charset: "utf8", force: :cascade do |t|

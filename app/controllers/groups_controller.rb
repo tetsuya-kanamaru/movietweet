82f95d75cfe0a2a4cfd9c44f60144b8ef4be00b7
tetsuya_kanamaru <67969845+tetsuya-kanamaru@users.noreply.github.com>
2021-04-12 16:13:24 +0900

    グループ作成フォームの作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,9 +5,17 @@ class GroupsController < ApplicationController
   end
   
   def create
-    
-    binding.pry
-    
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    # else
+    #   render: new
+    end
   end
   
+  private
+
+  def group_params
+    params.require(:group).permit(:name, user_ids:[])
+  end
 end

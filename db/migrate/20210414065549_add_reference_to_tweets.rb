f34ed731d80150ec97a89b340f1afb5413caef91
tetsuya_kanamaru <67969845+tetsuya-kanamaru@users.noreply.github.com>
2021-04-16 11:16:49 +0900

    グループ関連のマイグレーションファイルの削除

--- db/migrate/20210414065549_add_reference_to_tweets.rb
+++
@@ -1,5 +0,0 @@
-class AddReferenceToTweets < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :tweets, :group, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_065549) do
+ActiveRecord::Schema.define(version: 2021_04_08_050355) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -50,29 +50,12 @@ ActiveRecord::Schema.define(version: 2021_04_14_065549) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "group_users", charset: "utf8", force: :cascade do |t|
-    t.bigint "group_id"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["group_id"], name: "index_group_users_on_group_id"
-    t.index ["user_id"], name: "index_group_users_on_user_id"
-  end
-
-  create_table "groups", charset: "utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "tweets", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.bigint "group_id", null: false
-    t.index ["group_id"], name: "index_tweets_on_group_id"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
@@ -93,8 +76,5 @@ ActiveRecord::Schema.define(version: 2021_04_14_065549) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "tweets"
   add_foreign_key "comments", "users"
-  add_foreign_key "group_users", "groups"
-  add_foreign_key "group_users", "users"
-  add_foreign_key "tweets", "groups"
   add_foreign_key "tweets", "users"
 end

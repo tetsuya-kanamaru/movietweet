844a94290fc61ab3bd5cc367c7e37f4316305bd8
tetsuya_kanamaru <67969845+tetsuya-kanamaru@users.noreply.github.com>
2021-05-26 10:21:46 +0900

    local環境でのBasic認証設定
    環境変数設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_050355) do
+ActiveRecord::Schema.define(version: 2021_05_08_052951) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false

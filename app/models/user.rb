8b6482643e737e92625f35ecad239bb4e176d6f3
tetsuya_kanamaru <67969845+tetsuya-kanamaru@users.noreply.github.com>
2021-05-28 13:55:01 +0900

    userモデルの単体テストコード完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 10f }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6}
   validates :password_confirmation, presence: true
   
   def remember_me
